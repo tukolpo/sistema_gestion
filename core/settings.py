5149c3e64ac879ad86153ae2e9eb51032cfe1249
Jostin Oropeza <apariciodaniel676@gmail.com>
2026-05-25 22:12:53 -0400

    data: Guardando mis cambios antes de sincronizar

--- core/settings.py
+++ core/settings.py
@@ -59,8 +59,12 @@ WSGI_APPLICATION = "core.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+       'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'vice_gestion',
+        'USER': 'postgres',
+        'PASSWORD': '0808',   
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
